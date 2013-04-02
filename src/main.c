40a82842f3a0eaa24aafbf9391eb87d888ad0abb
kylaste <kylaste@d86fee5e-fa9b-e6fc-174c-ce4309e88b24>
2013-04-02 13:37:07 +0000

    le debut de terrain et case (et developpementmais moins important...)

--- src/main.c
+++ src/main.c
@@ -3,12 +3,21 @@
 #include <assert.h>
 #include "objet.h"
 #include "personnage.h"
+#include "terrain.h"
 
 /**
 * \author RODARIE Dimitri, VERSAEVEL Romain, FLORES Isabelle
 */
 
-int mainmain (int argc, char *argv[])
+int main (int argc, char *argv[])
 {
+	Terrain* terrain;
+	
+	initTerrain(terrain);
+	
+	afficheTerrain(terrain);
+	
+	libereTerrain(terrain);
+	
     return 0;
 }
