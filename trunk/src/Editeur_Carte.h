6d2a4c71ca923d8c3cfda0f8f03ab6e8a9fd5deb
d.rodarie@gmail.com <d.rodarie@gmail.com@d86fee5e-fa9b-e6fc-174c-ce4309e88b24>
2013-05-01 15:46:14 +0000

    oups ... le .h en + c'est toujours mieux...

---
+++ trunk/src/Editeur_Carte.h
@@ -0,0 +1,16 @@
+#ifndef __Editeur_Carte_h__
+#define __Editeur_Carte_h__
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* \author RODARIE Dimitri, VERSAEVEL Romain, FLORES Isabelle
+*/
+
+/**
+* \brief Preconditions :
+* <br /> Postconditions : Ouvre une fenêtre SDL permettant de créer des terrains
+*/
+int mainEdit ();
+
+#endif
