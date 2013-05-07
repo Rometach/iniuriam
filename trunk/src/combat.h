db1259bad9b72c64bf337d0999c99be5cb2db1b1
d.rodarie@gmail.com <d.rodarie@gmail.com@d86fee5e-fa9b-e6fc-174c-ce4309e88b24>
2013-05-07 06:10:30 +0000

    Combat.c, combat.h

---
+++ trunk/src/combat.h
@@ -0,0 +1,29 @@
+#ifndef __combat_h__
+#define __combat_h__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "personnage.h"
+#include "terrain.h"
+
+/**
+* \author RODARIE Dimitri, VERSAEVEL Romain, FLORES Isabelle
+*/
+
+typedef struct
+{
+    Personnage* perso;
+    char camp;
+    int ordre;
+    int posX;
+    int posY;
+    char orientation; /*Correspond à l'orientation du regard du combattant sur le terrain :
+                        1 vers la gauche
+                        2 vers le haut
+                        3 vers la droite
+                        4 vers le bas*/
+} Combattant;
+
+int mainCombat();
+
+#endif
